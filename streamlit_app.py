3b736652e8a386936d87c7af0a152e3e3316efe5
mshumer <41550495+mshumer@users.noreply.github.com>
2022-07-08 19:22:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,29 @@
-# Added to as an example of how to track your app using Umami
 import streamlit as st
-import streamlit.components.v1 as components
+import requests
 
-components.html(f'<script async defer data-website-id="bd4bc597-839f-4f88-8c86-61b5785b8e03" src="https://umami-demo-production.up.railway.app/umami.js"></script>')
 
-st.write("Streamlit and Umami integration demo.")
+# Title
+st.title("AutoAds")
+
+# Input
+text = st.text_area("Enter market to tackle")
+
+if st.button("Generate Ads and Keywords"):
+    url = "https://flask-production-b223.up.railway.app/get_ads"
+    payload={'market': text}
+
+    response = requests.request("POST", url, data=payload)
+
+    # break up the json response
+    response_json = response.json()
+    headlines = response_json["Headlines"]
+    descriptions = response_json["Descriptions"]
+    keywords = response.json["Keywords"]
+
+    # display the translation and reasoning
+    st.write('Headlines')
+    st.write(headlines)
+    st.write('Descriptions')
+    st.write(descriptions)
+    st.write('Keywords')
+    st.write(keywords)
